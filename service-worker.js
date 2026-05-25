ded9adaddb495244dfb62e2256b9493da977cbb6
vladimi251077 <golovin-vla@yandex.ru>
2026-05-25 21:37:17 +0300

    Cache latest compact panel assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='debt-diff-app-v3-panels-20260524';
-const FILES=['./','./index.html','./style.css?v=20260524-panels1','./app.js?v=20260524-panels1','./manifest.json','./icon.svg'];
+const CACHE='debt-diff-app-v4-panels2-20260524';
+const FILES=['./','./index.html','./style.css?v=20260524-panels2','./app.js?v=20260524-panels2','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(c=>c||caches.match('./index.html'))))});
