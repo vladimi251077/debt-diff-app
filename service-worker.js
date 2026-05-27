eb9b47ce91330cb0bdd66b09160df8cbd860cad1
vladimi251077 <golovin-vla@yandex.ru>
2026-05-27 22:00:45 +0300

    Refresh service worker cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='debt-diff-app-v5-autobackup-20260524';
+const CACHE='debt-diff-app-v6-autobackup-20260527';
 const FILES=['./','./index.html','./style.css?v=20260524-autobackup1','./app.js?v=20260524-autobackup1','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
